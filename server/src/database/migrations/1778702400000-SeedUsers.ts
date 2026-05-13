28b6a6008b551ddd8fe729615a5e4fe469f5de1f
yael-katzav <yaelkatzav49@gmail.com>
2026-05-14 00:41:23 +0300

    feat: update user seed data in migration to include specific names

--- server/src/database/migrations/1778702400000-SeedUsers.ts
+++ server/src/database/migrations/1778702400000-SeedUsers.ts
@@ -6,16 +6,17 @@ export class SeedUsers1778702400000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       INSERT INTO users (name) VALUES
-      ('Alice'),
-      ('Bob'),
-      ('Charlie')
+      ('Yael'),
+      ('Eyal'),
+      ('Nadav'),
+      ('Shelly'),
     `)
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
       DELETE FROM users
-      WHERE name IN ('Alice', 'Bob', 'Charlie')
+      WHERE name IN ('Yael', 'Eyal', 'Nadav', 'Shelly')
     `)
   }
 }
